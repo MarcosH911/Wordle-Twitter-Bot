3fc1cb8545e842718369e72ee89051e918746971
Marcos Hernanz <marcosh.craft@gmail.com>
2022-02-16 22:38:08 +0100

    Add track of wordle id

--- main.py
+++ main.py
@@ -1,22 +1,29 @@
 import tweepy
+from keys import *
 
 
-consumer_key = "P5Q5GJvYatyRBEt4tx4iLOpfU"
-consumer_secret = "8jouxLezINeAUzMuc3vXb15Yge6FvEonII1o2s1NH0EeptPJt4"
-access_token = "1492973847053803522-4zg3t9UZwTLjDsMaqeThuW3mHcGRw9"
-access_token_secret = "ZSrQoGHKVjMIvK3s9xk0T9Yk9CiMOGwAUFbAyYLugpGRQ"
-
-
-auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
+auth = tweepy.OAuthHandler(API_KEY, API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-try:
-    api.verify_credentials()
-    print("Authentication OK")
-except:
-    print("Error during authentication")
+FILE_NAME = "wordle_id.txt"
 
-# myStreamListener = MyStreamListener()
-# myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-# myStream.filter(track=["news"])
+
+def retrieve_wordle_id(file_name):
+    f_read = open("wordle_id.txt", 'r')
+    wordle_id = int(f_read.read())
+    f_read.close()
+    return wordle_id
+
+
+def store_wordle_id(file_name, wordle_id):
+    f_write = open("wordle_id.txt", 'w')
+    f_write.write(strint(text) + 1)
+    f_write.close()
+    return
+
+
+# store_wordle_id()
+
+# api.update_status('This is my first tweet with the API!\n:D')
+# #Wordle  #Wordle241
